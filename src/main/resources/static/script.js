be5be8bc4b3169470a347734a48ba0fbe93a3000
dajoh2062 <dajoh2062@oslomet.no>
2024-02-18 15:45:37 +0100

    validation
    input

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,6 +1,5 @@
-
-
 let biletter = [];
+
 
 function kjopbilett() {
     const film = document.getElementById("velgFilm").value;
@@ -10,53 +9,58 @@ function kjopbilett() {
     const email = document.getElementById("email").value;
 
     if (!film || !fornavn || !etternavn || !telefon || !email) {
-        alert("Vennligst fyll ut alle feltene")
+        alert("Vennligst fyll ut alle feltene");
         return;
     }
-    const bilett = {film, fornavn, etternavn, telefon, email}
+    const patternFornavn = /^[A-Za-z]+$/;
+    if (!patternFornavn.test(fornavn)) {
+        alert("Fornavnet er ikke gyldig");
+        return;
+    }
+
+    const bilett = { film, fornavn, etternavn, telefon, email };
 
     biletter.push(bilett);
-
     visbiletter();
     tomfelter();
-
 }
+
 function visbiletter() {
-    const billetListe = document.getElementById('biletter');
+    const billetListe = document.getElementById("biletter");
     billetListe.innerHTML = "";
 
     for (let indeks = 0; indeks < biletter.length; indeks++) {
         const bilett = biletter[indeks];
 
-        const li = document.createElement('li');
-        li.textContent = "Billett " + (indeks + 1) + ": " + bilett.film + " - " + bilett.fornavn + bilett.etternavn + " (" + bilett.email + ", " + bilett.telefon + ")";
+        const li = document.createElement("li");
+        li.textContent =
+            "Billett " +
+            (indeks + 1) +
+            ": " +
+            bilett.film +
+            " - " +
+            bilett.fornavn +
+            " " +
+            bilett.etternavn +
+            " (" +
+            bilett.email +
+            ", +47 " +
+            bilett.telefon +
+            ")";
         billetListe.appendChild(li);
     }
-
-
-
 }
+
 function tomfelter() {
+    document.getElementById("antall").value = "";
     document.getElementById("velgFilm").value = "";
     document.getElementById("fornavn").value = "";
     document.getElementById("etternavn").value = "";
     document.getElementById("telefon").value = "";
     document.getElementById("email").value = "";
-
 }
 
 function slettBiletter() {
     biletter = [];
     visbiletter();
-}
-function validateInput(input) {
-    const errorSpan = input.nextElementSibling; // Assuming the error span follows the input
-    const isValid = input.checkValidity();
-
-    if (!isValid) {
-        input.setCustomValidity(input.title);
-    } else {
-        input.setCustomValidity("");
-        errorSpan.textContent = "Wrong";
-    }
 }
